5eb027ee13c0008facd26ecf30c443ac06ab742c
Will <william@imotion-info.com>
2020-01-29 01:08:34 -0300

    Add middlewares

--- modulo01/server/index.js
+++ modulo01/server/index.js
@@ -5,22 +5,47 @@ server.use(express.json());
 
 const users = ["João", "José", "Maria"];
 
+server.use((req, res, next) =>{
+    console.time("Request");
+    console.log(`Método ${req.method}; URL ${req.url}`);
+
+    next();
+
+    console.timeEnd("Request");
+});
+
+function checkUsersExists(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({error: 'name not found on request body'});
+    }
+    return next();
+}
+
+function checkUserInArray(req, res, next){
+    const user = users[req.params.index];
+    if(!user){
+        return res.status(400).json({error:'User does not exist'});
+    }
+    req.user = user;
+
+    return next();
+}
+
 server.get("/users", (req, res)=>{
     return res.json(users);
 })
 
-server.get("/users/:index", (req, res)=>{
-    const {index} = req.params;
-    return res.json(users[index]);
+server.get("/users/:index",checkUserInArray, (req, res)=>{
+    return res.json(req.user);
 })
 
-server.post("/users", (req, res)=>{
+server.post("/users", checkUsersExists, (req, res)=>{
     const {name} = req.body;
     users.push(name);
     return res.json(users);
 })
 
-server.put("/users/:index", (req, res)=>{
+server.put("/users/:index", checkUserInArray, checkUsersExists, (req, res)=>{
     const {name} = req.body;
     const {index} = req.params;
 
@@ -28,7 +53,7 @@ server.put("/users/:index", (req, res)=>{
     return res.json(users);
 })
 
-server.delete("/users/:index", (req, res)=>{
+server.delete("/users/:index", checkUserInArray,(req, res)=>{
     const {index} = req.params;
 
     users.splice(index, 1);
